ef405d04a57f58e7d4509df2d1f949701549de56
vsnyc <vsnyc@users.noreply.github.com>
2018-06-21 15:45:09 -0400

    Added script to process reference images

---
+++ scripts/process_ref_images.py
@@ -0,0 +1,10 @@
+import zmq
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:" + str(5555))
+
+for i in range(1, 5):
+  socket.send('jedi_pose{}.jpg\0'.format(i))
+  message = socket.recv()
+  print(message)
