5d88650e29fe1f946bd5105dd0de7ebc9f56f6d0
vsnyc <vsnyc@users.noreply.github.com>
2018-06-21 15:36:18 -0400

    Updated run_server script

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-git checkout https://github.com/sshvans/are-you-the-jedi-master.git
-cd are-you-the-jedi-master
 cp /tmp/ddr_config.props .
 nohup python -m op_server.sqs_poller > /home/ubuntu/sqs_poller.log 2>&1 &
 cd /home/ubuntu/openpose
