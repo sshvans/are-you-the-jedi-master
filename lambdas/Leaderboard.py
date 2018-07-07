4e56ee20d1bc1193d46df34629a7cef7d8c1d304
vsnyc <vsnyc@users.noreply.github.com>
2018-07-07 14:38:52 -0400

    Rounded up scores in Leaderboard

--- lambdas/Leaderboard.py
+++ lambdas/Leaderboard.py
@@ -3,6 +3,7 @@ import json
 import logging
 import operator
 import os
+import math
 
 import boto3
 
@@ -60,7 +61,7 @@ class DecimalEncoder(json.JSONEncoder):
   def default(self, o):
     if isinstance(o, decimal.Decimal):
       if o % 1 > 0:
-        return float(o)
+        return math.ceil(float(o))
       else:
         return int(o)
     return super(DecimalEncoder, self).default(o)
